c2fb157da6bf6a39ea4a88c7897686aacdeb82ac
Greg Hormann <hormann.gj@pg.com>
2021-02-22 17:13:06 -0500

    use color schemes plugin

--- website/my.js
+++ website/my.js
@@ -1,34 +1,29 @@
 
-var masterData = 
-{
-    backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-    ],
-    borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-    ],
-
+function sortByValue(a, b) {
+    return b.value - a.value;
 }
 
+// Creates a pi Chart from a "last seen in" format.
 function drawPieChart(ctx, input, level) {
     let textLabel = input.description;
     let chartLabels = [];
     let chartData = [];
-    
+
+    let sortable = [];
+
     for (const [key, value] of Object.entries(input.data)) {
-        chartLabels.push(key);
-        chartData.push(value[level]);
-      }
+        sortable.push({
+            label: key,
+            value: value[level]
+        });
+    }
+    
+    sortable.sort(sortByValue);
+
+    sortable.forEach((row) => {
+        chartLabels.push(row.label);
+        chartData.push(row.value);
+    });
 
     var myChart = new Chart(ctx, {
         type: 'pie',
@@ -37,13 +32,17 @@ function drawPieChart(ctx, input, level) {
             datasets: [{
                 label: textLabel,
                 data: chartData,
-                backgroundColor: masterData.backgroundColor,
-                borderColor: masterData.borderColor,
                 borderWidth: 1
             }]
         },
         options: {
             responsive: true,
+            plugins: {
+                colorschemes: {
+                    // Options from https://nagix.github.io/chartjs-plugin-colorschemes/colorchart.html
+                    scheme: 'brewer.Paired12'
+                }
+            }
         }
     });
 }
