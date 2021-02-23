7a4c45f94673b05e42379791539244362e159928
Greg Hormann <hormann.gj@pg.com>
2021-02-22 20:28:39 -0500

    platformVariant

---
+++ statsCollector/handlers/platformVariant.js
@@ -0,0 +1,30 @@
+"use strict";
+const util = require("../lib/util.js");
+
+let myData = {};
+
+module.exports = [
+    {
+        name: "platformVariant",
+        description: "PI 2, 3, or 4",
+        reset: async () => {
+            myData = {};
+        },
+        results: async () => {
+            return myData;
+        },
+        currentHandler: async (obj) => {
+            let platform = "Not Defined";
+            if ("systemInfo" in obj) {
+                if ("platformVariant" in obj.systemInfo) {
+                    platform = obj.systemInfo.platformVariant;
+                }
+            }
+
+            if (!(platform in myData)) {
+                myData[platform] = util.newCountByAgeObject();
+            }
+            util.countByAge(myData[platform], obj);
+        },
+    },
+];
