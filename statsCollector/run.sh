9aa024c8f00c4cb320c1e1fe3c789cfd05c1b8b7
Greg Hormann <ghormann@thehormanns.net>
2021-10-09 15:00:28 +0000

    Make zip quiet

--- statsCollector/run.sh
+++ statsCollector/run.sh
@@ -5,7 +5,7 @@ do
     echo "Starting Process"
     node ./index.js
     echo "Starting Zip"
-    time find /statsdata -name "*.json" | zip -@ /statsdata/all_files.zip
+    time find /statsdata -name "*.json" | zip -@ -qq /statsdata/all_files.zip
     echo "Zip Done"
     sleep 3600 # An hour
 
