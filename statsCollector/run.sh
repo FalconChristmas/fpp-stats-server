be7c1ca6dacb4bbaafce3176f6a99c65f727f239
Greg Hormann <ghormann@gmail.com>
2022-02-19 09:56:14 -0500

    Avoid updating zip file

--- statsCollector/run.sh
+++ statsCollector/run.sh
@@ -5,7 +5,8 @@ do
     echo "Starting Process"
     node ./index.js
     echo "Starting Zip"
-    time find /statsdata -name "*.json" | zip -@ -qq /statsdata/all_files.zip
+    time (find /statsdata -name "*.json" | zip -@ -qq /statsdata/new_all_files.zip)
+    mv /statsdata/new_all_files.zip /statsdata/all_files.zip
     echo "Zip Done"
     sleep 3600 # An hour
 
